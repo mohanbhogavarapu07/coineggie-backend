8c5ed3a20181592270dcd6b37e34964cba9895b9
narayanabhogav <narayana.bhogavarapu@sasi.ac.in>
2025-06-22 11:41:31 +0530

    added cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:3000'],
+  origin: ['http://localhost:8080', 'http://localhost:3000','https://www.coineggie.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
